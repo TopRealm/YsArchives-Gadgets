2b3c4f595137611b38bddd6194b685aac4a431aa
海屿有燕 <zorua@vip.qq.com>
2025-03-07 01:14:58 +0800

    feat: 添加 ChineseDays 功能并更新相关依赖，移除不再使用的常量

--- src/ConfirmLogout/modules/util/refreshEventListener.ts
+++ src/ConfirmLogout/modules/util/refreshEventListener.ts
@@ -3,16 +3,11 @@ import {filterAlteredClicks} from 'ext.gadget.FilterAlteredClicks';
 const refreshEventListener = ($element: JQuery, clickListener: (event: JQuery.ClickEvent) => void): void => {
 	const hoverListener = (): void => {
 		$element.off('click');
-		$element.on(
-			'click',
-			filterAlteredClicks((event: JQuery.ClickEvent): void => {
-				clickListener(event);
-			})
-		);
+		$element.on('click', filterAlteredClicks(clickListener));
 	};
 	const hoverListenerWithDebounce: typeof hoverListener = mw.util.debounce(hoverListener, 200, true);
 
-	$element.on('mouseover touchstart', hoverListenerWithDebounce);
+	$element.on('mouseover touchstart', {passive: true}, hoverListenerWithDebounce);
 };
 
 export {refreshEventListener};
