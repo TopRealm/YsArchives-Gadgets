740ec07e234f9a5e2ca93886ef630b0d1d188644
海屿有燕 <zorua@vip.qq.com>
2025-03-07 02:38:34 +0800

    refactor: 优化Group-user样式，添加随机页面链接替换功能

---
+++ src/Group-user_JS/modules/replaceRandom.ts
@@ -0,0 +1,19 @@
+import {userIsInGroup} from 'ext.gadget.Util';
+
+const replaceRandom = ($body: JQuery<HTMLBodyElement>) => {
+	const {wgWikiID} = mw.config.get();
+	if (wgWikiID !== 'wiki-ysy') {
+		return;
+	}
+
+	if (
+		!userIsInGroup(['rnrsverify-confirmed', 'rnrsverify-exempt', 'steward', 'qiuwen', 'confirmed', 'autoconfirmed'])
+	) {
+		return;
+	}
+
+	const $randomPageLink = $body.find('#n-randompage a');
+	$randomPageLink.attr('href', '/wiki/Special:RandomPage');
+};
+
+export {replaceRandom};
