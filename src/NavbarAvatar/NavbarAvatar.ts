20359fe87f8d96c5791217486d6bb5cfc2059ed1
海屿有燕 <zorua@vip.qq.com>
2025-05-04 05:24:58 +0800

    fix(NavbarAvatar): update default avatar image URL

--- src/NavbarAvatar/NavbarAvatar.ts
+++ src/NavbarAvatar/NavbarAvatar.ts
@@ -5,7 +5,7 @@ if (username) {
 	username = username.replace(' ', '_'); // 避免名称包含空格时无法显示头像
 	imgsrc = `${mw.config.get('wgServer')}/extensions/Avatar/avatar.php?user=${username}`;
 } else {
-	imgsrc = `${mw.config.get('wgServer')}/images/avatars/default/default.gif`;
+	imgsrc = 'https://image.youshou.wiki/avatars/default/default.gif';
 }
 const $body: JQuery<HTMLBodyElement> = $('body');
 $body.find('.citizen-ui-icon.mw-ui-icon-userAvatar.mw-ui-icon-wikimedia-userAvatar').css({
