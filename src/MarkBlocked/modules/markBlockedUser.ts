771780a3fa239bb8eb10e6534dc841279be14044
海屿有燕 <zorua@vip.qq.com>
2025-11-04 20:19:31 +0800

    feat(MarkBlocked): implement old keys cleanup and optimize user link marking

--- src/MarkBlocked/modules/markBlockedUser.ts
+++ src/MarkBlocked/modules/markBlockedUser.ts
@@ -1,11 +1,10 @@
-import {markBlockedIPLinks, markBlockedUserLinks, markGlobalLockedUserLinks} from './util/markUserLinks';
+import {markBlockedIPLinks, markBlockedUserLinks} from './util/markUserLinks';
 import {generateUserLinks} from './util/generateUserLinks';
 
 const markBlockedUser = ($content: JQuery): void => {
 	// Find all "user" links and save them in userLinks : { 'users': [<link1>, <link2>, ...], 'user2': [<link3>, <link3>, ...], ... }
 	const userLinks: Record<string, JQuery[]> = generateUserLinks($content);
 
-	markGlobalLockedUserLinks(userLinks);
 	markBlockedUserLinks(userLinks);
 	markBlockedIPLinks(userLinks);
 };
