71a5e4c4f892e6a1a009a39a2f999627886cf8b4
海屿有燕 <zorua@vip.qq.com>
2026-01-25 13:52:41 +0800

    fix: enhance text spacing and user avatar loading

--- src/UserLinkAvatar/UserLinkAvatar.ts
+++ src/UserLinkAvatar/UserLinkAvatar.ts
@@ -2,9 +2,11 @@ import './UserLinkAvatar.less';
 
 /* eslint-disable unicorn/no-array-for-each */
 document.querySelectorAll('.mw-userlink').forEach((item) => {
+	const userName = item.textContent;
 	const img = document.createElement('img');
 	img.classList.add('userlink-avatar');
-	img.src = `${mw.config.get('wgScriptPath')}/extensions/Avatar/avatar.php?user=${item.textContent}`;
-	img.alt = ''; // alt="" for decorative images
+	img.loading = 'lazy';
+	img.src = `${mw.config.get('wgScriptPath')}/extensions/Avatar/avatar.php?user=${userName}`;
+	img.alt = `用户${userName}的头像`;
 	item.prepend(img);
 });
